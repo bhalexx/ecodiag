78b96e60672a644f35bda04021718eb3fb0f91df
Alexandra Dubreucq <a.dubreucq@choosit.com>
2024-08-19 08:11:06 +0200

    theme switcher

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { StyleguideComponent } from './styleguide/styleguide.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'styleguide',
+        component: StyleguideComponent,
+    },
+];
---
+++ client/src/app/styleguide/styleguide.component.ts
@@ -0,0 +1,8 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'app-home',
+    templateUrl: './styleguide.template.html',
+    standalone: true,
+})
+export class StyleguideComponent {}
